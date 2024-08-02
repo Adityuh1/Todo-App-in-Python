c73e7d5b2b91ccbfed926178878a3088e2e87394
Jadaon <adityajadaon010@gmail.com>
2024-08-02 15:00:17 +0530

    completed the Complete and Exit buttom

--- functions.py
+++ functions.py
@@ -1,13 +1,13 @@
 FILEPATH = "todos.txt"
 
-def get_todo(filepath = FILEPATH):
-    """READS THE FILE AND RETURNS THE todo list."""
+def get_todo(filepath=FILEPATH):
+    """READS THE FILE AND RETURNS THE todo-list."""
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todo(todos_arg, filepath = FILEPATH):
+def write_todo(todos_arg, filepath=FILEPATH):
     """Writes To-Do items list in a text file."""
     with open(filepath, 'w') as file_local2:
         file_local2.writelines(todos_arg)
